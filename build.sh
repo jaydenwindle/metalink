c8186d6be88e9326017722fd28d679ca0848ec1e
Jayden Windle <jaydenwindle@gmail.com>
2021-05-02 18:41:08 -0600

    added tailwind build step, reverted space url

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ set -o errexit
 
 poetry install
 
+python manage.py tailwind build
+
 python manage.py collectstatic --no-input
 python manage.py migrate
--- metalink/settings.py
+++ metalink/settings.py
@@ -159,7 +159,7 @@ if "RENDER" in os.environ:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = 'metalink'
-    AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com/'
+    AWS_S3_ENDPOINT_URL = 'https://metalink.nyc3.digitaloceanspaces.com/'
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
     }
