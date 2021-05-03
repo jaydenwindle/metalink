31462ec632fff1c4dc221fc422d48222077fc7c4
Jayden Windle <jaydenwindle@gmail.com>
2021-05-02 18:23:42 -0600

    added digitalocean spaces for file storage

--- metalink/settings.py
+++ metalink/settings.py
@@ -156,5 +156,19 @@ if "RENDER" in os.environ:
         )
     }
 
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = 'metalink'
+    AWS_S3_ENDPOINT_URL = 'https://metalink.nyc3.digitaloceanspaces.com/'
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
+    AWS_LOCATION = 'static'
+
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
+    STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
