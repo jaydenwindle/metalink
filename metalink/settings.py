6ae0cc38ee15e8500e277920507424a5249a3b5f
Jayden Windle <jaydenwindle@gmail.com>
2021-05-02 22:22:14 -0600

    fixed 400 error

--- metalink/settings.py
+++ metalink/settings.py
@@ -144,10 +144,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'core/static'),
-]
-
 if "RENDER" in os.environ:
     DEBUG = False
     RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
@@ -174,7 +170,6 @@ if "RENDER" in os.environ:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'theme/static'),
-        os.path.join(BASE_DIR, 'core/static'),
     ]
     STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
